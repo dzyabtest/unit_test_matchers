437708d48bb39db20b11435a0638b98cc10672a9
dzyabtest <dzyab@mail.ru>
2023-06-27 01:26:30 +0300

    Устранение ошибки версий вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
